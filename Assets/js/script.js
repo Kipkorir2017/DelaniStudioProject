71f9ea93a5b295ebde78f2302c0dd3bb380fd711
Kipkorir2017 <pkobenj41@gmail.com>
2021-07-23 16:18:33 +0300

    add form validation

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -17,32 +17,29 @@ $(document).ready(function(){
     $("#img3").toggle();
     })
 });
-// fetching inputs and alerting feedback
-$(document).ready(function(){
-    $("form").submit(function(event){
-        var name=$("#name").val();
-        var email=$("#email").val();
-        alert(name+" we have received your message.Thank you for reaching out to us.");
-        event.preventDefault();
-    })
-})
-
 //  Name and Email Validation
 function validateForm(){
-    var name = document.getElementById("name").Value;
+    var name = document.getElementsById("name").Value;
     var email = document.getElementById("email").value;
     atposition=email.indexOf("@");
     dotposition =email.lastIndexOf(".");
-    console.log(name)
-    if(atposition < 2 || dotposition <2){
+     if(name=" "){
+        alert ("Name must be filled out");
+        return false;
+    }
+    else if(atposition < 2 || dotposition <2){
         alert("invalid email");
         return false;
     }
-    else if(name.length<2 || name.length>= 50){
-        alert ("Enter a correct Name");
-        return false;
-    }
-    else{
-        return true;
-    }
+   
 }
+// fetching inputs and alerting feedback
+$(document).ready(function(){
+    $("form").submit(function(){
+        var name=$("#name").val();
+        var email=$("#email").val();
+        alert(name+",  we have received your message.Thank you for reaching out to us.");
+        return true;
+    })
+})
+
