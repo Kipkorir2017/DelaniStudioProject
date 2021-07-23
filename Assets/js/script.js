b3b67e37d2bc57ca0d17633dd625e86f6c3425cc
Kipkorir2017 <pkobenj41@gmail.com>
2021-07-23 12:24:17 +0300

    added media querries and js validation

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,4 @@
+// Using Jquery to toggle What we do
 $(document).ready(function(){
     $(".one").click(function(){
     $(".description").toggle()
@@ -16,3 +17,22 @@ $(document).ready(function(){
     $("#img3").toggle();
     })
 });
+//  Name and Email Validation
+function validateForm(){
+    var name = document.getElementById("name").Value;
+    var email = document.getElementById("email").value;
+    atposition=email.indexOf("@");
+    dotposition =email.lastIndexOf(".");
+    console.log(name)
+    if(atposition < 2 || dotposition <2){
+        alert("invalid email");
+        return false;
+    }
+    else if(name.length <=5 || name.length >= 50){
+        alert ("Enter a correct Name");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
