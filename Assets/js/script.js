93dc9798a0bd466b736572fb9148bf174aeea499
Kipkorir2017 <pkobenj41@gmail.com>
2021-07-23 17:07:17 +0300

    rectified javascript form Validation

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,23 @@
-// Using Jquery to toggle What we do
+// javascript validating form
+function validateForm(){
+    var name= document.getElementById("name1").value;
+    var email= document.getElementById("email1").value;
+    atposition=email.indexOf("@");
+    dotposition =email.lastIndexOf(".");
+    if(atposition< 2 || dotposition - atposition<2){
+        alert("invalid email");
+        return false;
+    }
+    else if(name==" "){
+        alert("Name must be filled out");
+        return false;
+    }
+    else{
+        alert(name+",Your Message has been recieved.Thank you for reaching out to us.")
+    }
+}
+
+// Using Jquery to toggle
 $(document).ready(function(){
     $(".one").click(function(){
     $(".description").toggle()
@@ -17,29 +36,4 @@ $(document).ready(function(){
     $("#img3").toggle();
     })
 });
-//  Name and Email Validation
-function validateForm(){
-    var name = document.getElementsById("name").Value;
-    var email = document.getElementById("email").value;
-    atposition=email.indexOf("@");
-    dotposition =email.lastIndexOf(".");
-     if(name=" "){
-        alert ("Name must be filled out");
-        return false;
-    }
-    else if(atposition < 2 || dotposition <2){
-        alert("invalid email");
-        return false;
-    }
-   
-}
-// fetching inputs and alerting feedback
-$(document).ready(function(){
-    $("form").submit(function(){
-        var name=$("#name").val();
-        var email=$("#email").val();
-        alert(name+",  we have received your message.Thank you for reaching out to us.");
-        return true;
-    })
-})
 
